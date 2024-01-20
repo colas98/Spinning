7c4dd13381111d4ee41fde823abf66cddf70dd77
colas98 <colas98@gmail.com>
2024-01-20 12:18:51 -0300

    adding all

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np 
+# import pandas as pd
+
+
+class Spinning(): 
+    def __init__(self):
+        self.test = 1 
+    def print_test(self):
+        print(self.test)
+
+Spin = Spinning()
+Spin.print_test()
